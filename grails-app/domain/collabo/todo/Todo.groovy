92478d3d34eb4c540c62dde02e727606d66741cc
Teyim Pila <teyim.pila@gmail.com>
2018-02-05 10:02:28 +0100

    initial commit

---
+++ grails-app/domain/collabo/todo/Todo.groovy
@@ -0,0 +1,34 @@
+package collabo.todo
+
+class Todo {
+
+    String name
+    String note
+    Date createdDate
+    Date dueDate
+    Date completedDate
+    String priority
+    String status
+    User owner
+    Category category
+
+    /**
+     * Defines Relationships
+     */
+    static belongsTo = [User, Category]
+
+
+    static constraints = {
+        name(blank: false)
+        createdDate()
+        priority()
+        status()
+        note(maxSize: 1000, nullable: true)
+        completedDate(nullable: true)
+        dueDate(nullable: true)
+    }
+
+    String toString() {
+        name
+    }
+}
---
+++ grails-app/init/collabo/todo/BootStrap.groovy
@@ -0,0 +1,9 @@
+package collabo.todo
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
