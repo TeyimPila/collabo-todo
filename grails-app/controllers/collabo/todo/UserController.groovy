4fb7413a6bb3439c340460f480fe466b0550b81f
Teyim Pila <teyim.pila@gmail.com>
2018-02-07 15:19:02 +0100

    prevent users from editing other users

--- grails-app/controllers/collabo/todo/UserController.groovy
+++ grails-app/controllers/collabo/todo/UserController.groovy
@@ -11,7 +11,7 @@ class UserController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond userService.list(params), model:[userCount: userService.count()]
+        respond userService.list(params), model: [userCount: userService.count()]
     }
 
     def show(Long id) {
@@ -31,7 +31,7 @@ class UserController {
         try {
             userService.save(user)
         } catch (ValidationException e) {
-            respond user.errors, view:'create'
+            respond user.errors, view: 'create'
             return
         }
 
@@ -45,6 +45,20 @@ class UserController {
     }
 
     def edit(Long id) {
+
+        if (session.user.id != id) {
+            flash.message = "You can only edit yourself"
+            redirect(action: 'index')
+            return
+        }
+
+        def user = userService.get(id)
+
+        if (!user) {
+            notFound()
+            return
+        }
+
         respond userService.get(id)
     }
 
@@ -57,7 +71,7 @@ class UserController {
         try {
             userService.save(user)
         } catch (ValidationException e) {
-            respond user.errors, view:'edit'
+            respond user.errors, view: 'edit'
             return
         }
 
@@ -66,7 +80,7 @@ class UserController {
                 flash.message = message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), user.id])
                 redirect user
             }
-            '*'{ respond user, [status: OK] }
+            '*' { respond user, [status: OK] }
         }
     }
 
@@ -81,9 +95,9 @@ class UserController {
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), id])
-                redirect action:"index", method:"GET"
+                redirect action: "index", method: "GET"
             }
-            '*'{ render status: NO_CONTENT }
+            '*' { render status: NO_CONTENT }
         }
     }
 
@@ -93,7 +107,7 @@ class UserController {
                 flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
                 redirect action: "index", method: "GET"
             }
-            '*'{ render status: NOT_FOUND }
+            '*' { render status: NOT_FOUND }
         }
     }
 
