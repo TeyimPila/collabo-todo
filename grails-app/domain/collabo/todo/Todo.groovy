6f6950911f4ac5bae0cb7be0d8a0f5ed2ffe383b
Teyim Pila <teyim.pila@gmail.com>
2018-02-08 22:22:43 +0100

    update todo add new attributes

--- grails-app/domain/collabo/todo/Todo.groovy
+++ grails-app/domain/collabo/todo/Todo.groovy
@@ -9,6 +9,7 @@ class Todo {
     Date createdDate
     Date dueDate
     Date completedDate
+    Date lastModifiedDate
     String priority
     String status
     byte[] associatedFile
@@ -28,20 +29,25 @@ class Todo {
     static constraints = {
         name(blank: false)
         createdDate()
+        lastModifiedDate()
         priority()
         status()
         note(maxSize: 1000, nullable: true)
         completedDate(nullable: true)
         dueDate(nullable: true)
-        associatedFile(nullable:true)
+        associatedFile(nullable: true)
     }
 
 
-        static mapping = {
-            id generator:'uuid'
-        }
+    def beforeInsert = {
+        createdDate = new Date()
+        lastModifiedDate = new Date()
+    }
 
 
+    def beforeUpdate = {
+        lastModifiedDate = new Date()
+    }
 
     String toString() {
         name
