d561aa23dbf0d225371c3e64cacee1e45682c375
Teyim Pila <teyim.pila@gmail.com>
2018-02-22 15:07:48 +0100

    write tests for the user domain class

--- src/test/groovy/collabo/todo/UserSpec.groovy
+++ src/test/groovy/collabo/todo/UserSpec.groovy
@@ -11,8 +11,30 @@ class UserSpec extends Specification implements DomainUnitTest<User> {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Test CRUD operations on the User domain"() {
+        setup:
+            def user = new User(
+                    userName: 'testUser',
+                    firstName: "John",
+                    lastName: "Doe",
+                    password: "pass",
+                    email: "j.d@doe.com")
+
+            user.save()
+            def userId = user.id
+
+        when:
+            user.password = 'A new password'
+            user = User.get(userId)
+
+        then:
+            "A new password" == user.password
+            "John" == user.firstName
+
+        when:
+            user.delete()
+
+        then:
+            null == User.get(userId)
     }
 }
